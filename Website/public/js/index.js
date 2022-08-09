9a3022256cf768eb3e45b739718f70b0bb8055b8
Muhammad Juniarto <53488921+WallNutss@users.noreply.github.com>
2022-08-09 17:26:29 +0700

    fixing the static folder

---
+++ Website/public/js/index.js
@@ -0,0 +1,63 @@
+    // Getting the button
+    const control = document.querySelectorAll("button")
+    let ws;
+    let db;
+    const sendtime = document.getElementById("send")
+
+    function init(){
+        if(ws){
+            ws.onerror = ws.onopen = ws.onclose = null;
+            ws.close();
+        }
+        let HOST = location.origin.replace(/^http/, 'ws')
+        //let HOST = "ws://127.0.0.1:5000"
+        console.log(HOST)
+        ws = new WebSocket(HOST);
+        ws.onopen = ()=>{
+            console.log('Connection Open');
+        }
+        ws.onclose = function(){
+            ws = null;
+        }
+        const app = initializeApp(firebaseConfig);
+        console.log("Connected to firebase")
+        db = getDatabase(app)
+        console.log("Connected to database")
+    }
+    // Listening to each button controller, dekstop or on mobile
+    // Where each button have different respond, according to their ID
+    control.forEach(c =>{
+        c.addEventListener('mousedown',(e)=>{
+        if(!ws){
+            console.log("No Websocket Connection")
+        }
+        const command = e.target.id
+        ws.send(command)
+        })
+
+        c.addEventListener('mouseup',(e)=>{
+        if(!ws){
+            console.log("No Websocket Connection")
+        }
+        const command = "brake"
+        ws.send(command)
+        })
+
+        c.addEventListener('touchstart',(e)=>{
+            if(!ws){
+            console.log("No Websocket Connection")
+            }
+        const command = e.target.id
+        ws.send(command)
+        })
+
+        c.addEventListener('touchend',(e)=>{
+            if(!ws){
+            console.log("No Websocket Connection")
+            }
+        const command = "brake"
+        ws.send(command)
+        })
+    })
+
+    init();
--- Website/server.js
+++ Website/server.js
@@ -1,12 +1,12 @@
 const PORT = process.env.PORT || 5000; // Defining the PORT we used for communication server
 const INDEX =  '/index.html' // Page for the website
-const PATH = require('path')
 const express = require('express') // Node.js Application Framework
 const WebSocket = require('ws') // Websocket Protocol Library
 
 // Opening up the server using basic express library
 const server = express()
-    .use(express.static( __dirname + '/public'))
+    .use(express.static( __dirname + '/public')) // This is basically static, heroku does't support file storage in their 
+                                                //system so we have to make it static so style.css and index.js can be accessed in our website
     .get('/', (req,res) =>{
     res.sendFile(INDEX, {root : __dirname})}) //Basic Routing when website hit '/', respond serve index.html
     .listen(PORT, ()=> console.log(`Listening on ${PORT}`))
@@ -22,7 +22,8 @@ wss.on('connection', function connection(ws,req){ // Taking the event, when hand
     console.log("A Client has connected")
     console.log(`Now on Port ${PORT}`)
     ws.on('message', function incoming (data){ // Taking the event data incoming message, what to do going below
-        wss.clients.forEach(function each(client){ // If ws detect message, take all current clients connected
+        console.log(data.toString('utf-8'))
+        wss.clients.forEach(function each(client){ // If ws detect message, take all current clients connected    
             if(client != ws && client.readyState === WebSocket.OPEN){ //Ready state constant CONNECTION = 0 | OPEN = 1 | CLOSING = 2 | CLOSED = 3
                 client.send(data.toString('utf8')) // If it's true some client open and sending message, send that data to all
             }
